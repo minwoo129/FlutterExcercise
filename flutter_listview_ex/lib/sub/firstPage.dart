63fbcdb5b4061d0704ee83770745dec6b6c482b8
minwoo129 <key0129mw@naver.com>
2022-02-01 15:53:26 +0900

    listview_ex: 아이템, 기능 연결

--- flutter_listview_ex/lib/sub/firstPage.dart
+++ flutter_listview_ex/lib/sub/firstPage.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import '../animalItem.dart';
 
 class FirstApp extends StatelessWidget {
+  final List<Animal>? list;
+  FirstApp({Key? key, this.list}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text('첫 번째 페이지'),
+          child: ListView.builder(itemBuilder: (context, position) {
+            return GestureDetector(
+              child: Card(
+                child: Row(
+                  children: <Widget>[
+                    Image.asset(
+                      list![position].imagePath!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(list![position].animalName!)
+                  ],
+                ),
+              ),
+              onTap: () {
+                AlertDialog dialog = AlertDialog(
+                  content: Text('이 동물은 ${list![position].kind}입니다.', style: TextStyle(fontSize: 30.0),),
+                );
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => dialog
+                );
+              },
+            );
+
+          }, itemCount: list!.length,),
         ),
       ),
     );
