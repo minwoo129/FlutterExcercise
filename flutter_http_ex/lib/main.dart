92abdb22e15769e18f9a50be221bd1f609e394c2
minwoo129 <key0129mw@naver.com>
2022-02-02 14:29:35 +0900

    http_ex: 카카오 rest api 연결

--- flutter_http_ex/lib/main.dart
+++ flutter_http_ex/lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'dart:convert';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    // TODO: implement createHttpClient
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
@@ -36,10 +48,19 @@ class _HttpApp extends State<HttpApp> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          getJSONData();
         },
         child: Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url = "https://dapi.kakao.com/v3/search/book?target=title&query=doit";
+    var response = await http.get(Uri.parse(url),
+      headers: {"Authorization" : "KakaoAK 2555e6f9c3662df60f68f0aa9d75597d"}
+    );
+    print(response.body);
+    return "success";
   }
 }
