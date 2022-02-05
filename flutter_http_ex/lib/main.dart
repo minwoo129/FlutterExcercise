c7fbaab8e7376976f529ed090589ac6ddf90e308
minwoo129 <key0129mw@naver.com>
2022-02-05 21:45:43 +0900

    http_ex: 페이지네이션 추가(재시도 필요)

--- flutter_http_ex/lib/main.dart
+++ flutter_http_ex/lib/main.dart
@@ -37,6 +37,8 @@ class _HttpApp extends State<HttpApp> {
   String result = '';
   late List data;
   TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
 
   @override
   void initState() {
@@ -44,6 +46,17 @@ class _HttpApp extends State<HttpApp> {
     super.initState();
     data = new List.empty(growable: true);
     _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+          _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print('bottom');
+        page++;
+        getJSONData();
+      }
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -100,6 +113,8 @@ class _HttpApp extends State<HttpApp> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          data.clear();
           getJSONData();
         },
         child: Icon(Icons.file_download),
@@ -108,11 +123,11 @@ class _HttpApp extends State<HttpApp> {
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=${_editingController!.value.text}';
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController!.value.text}';
     var response = await http.get(Uri.parse(url),
       headers: {"Authorization" : "KakaoAK 2555e6f9c3662df60f68f0aa9d75597d"}
     );
-    //print(response.body);
+    print(response.body);
     setState(() {
       var dataConvertedToJSON = json.decode(response.body);
       List result = dataConvertedToJSON['documents'];
