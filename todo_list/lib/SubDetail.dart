b9fd36f3fc62ed0b063ddbceb79d94f0d5e2b101
minwoo129 <key0129mw@naver.com>
2022-02-06 16:49:07 +0900

    todo_list: navigator 연결

---
+++ todo_list/lib/SubDetail.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SubDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SubDetail();
+}
+
+class _SubDetail extends State<SubDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Detail Example')
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed('/second');
+            },
+            child: Text('두 번째 페이지로 이동하기')
+          )
+        ),
+      ),
+    );
+  }
+}
