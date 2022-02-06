8b2ab824921c330172528ca1c7914ca68e9fe1fb
minwoo129 <key0129mw@naver.com>
2022-02-06 17:05:59 +0900

    todo_list: 할일 보기 기능 추가

--- todo_list/lib/ThirdPage.dart
+++ todo_list/lib/ThirdPage.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 class ThirdDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String args = ModalRoute.of(context)!.settings.arguments.toString();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Third Page'),
         ),
       body: Container(
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('첫 번째 페이지로 돌아가기')
+          child: Text(
+            args,
+            style: TextStyle(fontSize: 30),
           ),
         ),
       ),
