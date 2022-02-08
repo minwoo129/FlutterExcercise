5574954c0d764fa8f9c5ed826eaf999329716924
minwoo129 <key0129mw@naver.com>
2022-02-08 21:31:37 +0900

    fileio_ex: 파일 입출력 연습

---
+++ flutter_fileio_ex/lib/file_app.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class FileApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FileApp();
+}
+
+class _FileApp extends State<FileApp> {
+  int _count = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readCountFile();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('File Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            '$_count',
+            style: TextStyle(fontSize: 40),
+          ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+          writeCountFile(_count);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void writeCountFile(int count) async {
+    var dir = await getApplicationDocumentsDirectory();
+    File(dir.path + '/count.txt').writeAsStringSync(count.toString());
+  }
+
+  void readCountFile() async {
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      var file = await File(dir.path + '/count.txt').readAsString();
+      print(file);
+      setState(() {
+        _count = int.parse(file);
+      });
+    }
+    catch(e) {
+      print(e.toString());
+    }
+  }
+}
