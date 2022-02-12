728d8afd01766fa9499ab76a91d694e9a2a3a837
minwoo129 <key0129mw@naver.com>
2022-02-12 16:14:06 +0900

    database_ex: 리스트 ui 연결(재작업 필요)

--- flutter_database_ex/lib/main.dart
+++ flutter_database_ex/lib/main.dart
@@ -52,13 +52,60 @@ class DatabaseApp extends StatefulWidget {
 }
 
 class _DatabaseApp extends State<DatabaseApp> {
+  Future<List<Todo>>? todoList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todoList = getTodos();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Database Example')
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              switch(snapshot.connectionState) {
+                case ConnectionState.none: 
+                  return CircularProgressIndicator();
+                case ConnectionState.waiting: 
+                  return CircularProgressIndicator();
+                case ConnectionState.active:
+                  return CircularProgressIndicator();
+                case ConnectionState.done:
+                  if(snapshot.hasData) {
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        Todo todo = (snapshot.data as List<Todo>)[index];
+                        return Card(
+                          child: Column(
+                            children: <Widget>[
+                              Text(todo.title!),
+                              Text(todo.content!),
+                              Text('${todo.active == 1 ? 'true' : 'false'}')
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: (snapshot.data as List<Todo>).length,
+                    );
+                  }
+                  else {
+                    return Text('No data');
+                  }
+              }
+              return CircularProgressIndicator();
+            },
+            future: todoList
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final todo = await Navigator.of(context).pushNamed('/add');
@@ -72,5 +119,20 @@ class _DatabaseApp extends State<DatabaseApp> {
   void _insertTodo(Todo todo) async {
     final Database database = await widget.db;
     await database.insert('todos', todo.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Todo>> getTodos() async {
+    final Database database = await widget.db;
+    final List<Map<String, dynamic>> maps = await database.query('todos');
+
+    return List.generate(maps.length, (i) {
+      int active = maps[i]['active'] == 1 ? 1 : 0;
+      return Todo(
+        title: maps[i]['title'].toString(),
+        content: maps[i]['content'].toString(),
+        active: active,
+        id: maps[i]['id']
+      );
+    });
   }
 }
